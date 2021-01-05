f038a8c53e525adee4dc49612863ac1f753522cf
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-01-05 15:27:19 -0800

    step 1 done

--- markov.js
+++ markov.js
@@ -7,7 +7,8 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
-    // MORE CODE HERE
+    this.chains = this.makeChains(words);
+
   }
 
   /** set markov chains:
@@ -15,14 +16,45 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // MORE CODE HERE
+  makeChains(words) {
+    let chains = {};
+
+    for (let i = 0; i < words.length; i++) {
+      let nextWord = words[i + 1] || null;
+      if (chains[words[i]]) {
+        chains[words[i]].push(nextWord);
+      }
+      else {
+        chains[words[i]] = [nextWord];
+      }
+    }
+    // console.log("Chains: ", chains)
+    return chains;
   }
 
 
   /** return random text from chains */
 
   getText(numWords = 100) {
-    // MORE CODE HERE
+    let startIndex = Math.floor(Math.random() * Object.keys(this.chains).length)
+    let words = [Object.keys(this.chains)[startIndex]];
+    for (let i = 0; i < numWords; i++) {
+      // find chain of possible words
+      let chain = this.chains[words[i]];
+      console.log(chain);
+      // pick one from chain
+      let nextWord = chain[Math.floor(Math.random() * chain.length)];
+      // console.log("next word:",nextWord);
+      if (nextWord === null){
+        break;
+      }
+      // add to words
+      words.push(nextWord);
+    }
+    // add words to text
+    return words.join(" ");
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat");
+console.log("I'm going to tell you a story: ", mm.getText())
